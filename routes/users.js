b94686aeef03685bbf76aa0cab5870156a9628fb
demoLiFe <1239545378@qq.com>
2022-11-12 12:34:25 +0800

    新增收藏接口 修改收货地址接口

--- routes/users.js
+++ routes/users.js
@@ -113,24 +113,87 @@ const res_data = {
   ] 
 };
 router.get('/api/address/list/get',function(req,res,next){ 
+  res_data.data
     res.send(res_data)
 })
 
-//地址管理新增编辑
+//地址管理编辑
 router.post('/api/address/list/edit',function(req,res,next){
     if(!!req){
         try {
            const index = res_data.data.findIndex(v => v.uid === req.body.uid)
            res_data.data[index] = req.body
+           res.send(res_data)
         }catch(err){
            console.log(err)
         }
     }
 });
 
+//地址管理新增
+router.post('/api/address/list/create',function(req,res,next){
+  if(!!req){
+      try{
+        res_data.data.push(req.body);
+        res.send(res_data)
+      }catch(err){
+        console.log(err)  
+      }
+  }
+})
+
+
 //地址管理删除
 router.post('/api/address/list/delete',function(req,res,next){
 
 });
+
+//收藏列表
+router.get('/api/collection/list',function(req,res,next){
+   const res_data = {
+     status:200,
+     data:[
+      {
+        id: nanoid(32),
+        title: '爱国者（aigo）64GB USB3.2 U盘 U330金属旋转系列 银色 快速传输 出色出众',
+        img: 'https://img10.360buyimg.com/mobilecms/s360x360_jfs/t1/42906/11/19323/53719/632839e5Eadddf33d/c19b7f2d6cac3684.jpg!q70.dpg.webp',
+        name:'syh的商店',
+        date:'10月1日'
+    },
+    {
+        id: nanoid(32),
+        title: '【无游戏】Svpmecn 华 为手机适用华强北S8插卡电话手表5G全网通青少年初高中生通话定位视频 黑色Y5【零钱支付+视频通话+精准定位+上课禁用】',
+        img: 'https://img10.360buyimg.com/mobilecms/s360x360_jfs/t1/139834/14/29859/67663/63281553Ee1031434/4df8d7556c9fe30d.jpg!q70.dpg.webp',
+        name:'syh的商店',
+        date:'10月10日'
+    },
+    {
+        id: nanoid(32),
+        title: '酷睿冰尊（ICE COOREL）A9 黑色笔记本散热器（电脑支架/笔记本散热架/散热垫/可调速/17英寸以下）',
+        img: 'https://img10.360buyimg.com/mobilecms/s360x360_jfs/t1/194831/6/28971/279367/63256908E7b4b9d6d/a401dacc90f8ea96.jpg!q70.dpg.webp',
+        name:'syh的商店',
+        date:'10月20日'
+    },
+    {
+        id: nanoid(32),
+        title: '雷柏（Rapoo） V500PRO 机械键盘 有线键盘 游戏键盘 104键混光键盘 吃鸡键盘 电脑键盘 黑色 黑轴',
+        img: 'https://img10.360buyimg.com/mobilecms/s360x360_jfs/t1/201709/31/16982/646706/61a5a319Eba43fa75/896ab6ece902cc7f.jpg!q70.dpg.webp',
+        name:'syh的商店',
+        date:'11月1日'
+    },
+    {
+        id: nanoid(32),
+        title: '闪魔 苹果11钢化膜 iphoneXR/XsMax/11promax手机膜高清非全屏抗蓝光防窥保护膜 XR/11【6.1英寸^加强版】2片+神器',
+        img: 'https://img10.360buyimg.com/mobilecms/s360x360_jfs/t1/178618/23/17933/539382/610b90f5E71b6dc6a/5506c079d831a0e8.jpg!q70.dpg.webp',
+        name:'syh的商店',
+        date:'11月10日'
+    },
+     ]
+   };
+   if(!!req){
+      res_data.data = res_data.data.filter(v=> v.title.indexOf(req.query.searchKey) > -1) 
+   }
+   res.send(res_data)
+})
 
 module.exports = router;
