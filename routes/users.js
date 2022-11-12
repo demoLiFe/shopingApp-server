9b63de35b95923836c611e23d5830eafb58c07bc
demoLiFe <1239545378@qq.com>
2022-11-12 16:59:50 +0800

    新增评论列表接口

--- routes/users.js
+++ routes/users.js
@@ -196,4 +196,122 @@ router.get('/api/collection/list',function(req,res,next){
    res.send(res_data)
 })
 
+
+//我的评价列表
+router.get('/api/appraise/list',function(req,res,next){
+  const res_data = {
+    status:200,
+    data:[]
+  };
+  const maps = [
+    {
+      type:0,
+      data:[
+        {
+          name:'闪魔旗舰店',
+          goodsName:'闪魔 苹果11钢化膜 iphoneXR/XsMax/11promax手机膜高清非全屏抗蓝光防窥保护膜 XR/11【6.1英寸^磨砂版】+神器',
+          goodsAttr:'X/XS/11Pro【5.8英寸磨砂版】+神器',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/169996/17/22109/366017/618394cdEc97d8da3/2243b840a308b9a4.jpg!q80.dpg'
+        },
+        {
+          name:'际麟金贝塔专卖店',
+          goodsName:'【可禁用】儿童智能电话手表初中高中生5G全网通视频通话GPS精准定位超长续航防水际麟 豪华版-耀黑色{精准定位+视频通话+无游戏}',
+          goodsAttr:'豪华版-耀黑色{精准定位+视频通话+无游戏}',
+          img:'https://img10.360buyimg.com/mobilecms/s360x360_jfs/t1/160700/10/32321/82594/636363fbEb1f897ee/fea79fef48acc63b.jpg!q70.dpg.webp'
+        },
+        {
+          name:'雷柏京东自营官方旗舰店',
+          goodsName:'雷柏（Rapoo） V500PRO 机械键盘 有线键盘 游戏键盘 104键混光键盘 吃鸡键盘 电脑键盘 黑色 黑轴',
+          goodsAttr:'V500PRO【混光茶轴】',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/205537/37/28060/206351/63633b1cE5ef4ecd0/46b50edf4b8afc67.jpg!q80.dpg'
+        },
+        {
+          name:'酷睿冰尊京东自营旗舰店',
+          goodsName:'酷睿冰尊（ICE COOREL）F1 笔记本抽风式散热器 （抽风式散热扇/笔记本配件/侧吸/吸风式散热)',
+          goodsAttr:'F1 5V供电【无极调速 直抽高温】',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/123110/16/26620/168602/624ed651Edecfb15d/7350732a19b20b7a.jpg!q80.dpg'
+        }
+      ]
+    },
+    {
+      type:1,
+      data:[
+        {
+          goodsName:'三只松鼠乳酸菌小伴侣面包 营养早餐代餐口袋手撕面包网红零食生日520g/箱',
+          goodsAttr:'【酸甜爆浆】乳酸菌520g 约26包',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/142219/2/31254/117001/636e48d6Eebf3b9ac/190e0a9a74a38cd9.jpg!q80.dpg'
+        },
+        {
+          goodsName:'乐事 薯片 超值三种口味组合装 104g*3',
+          goodsAttr:'104g*3 (黄瓜味+原味+青柠味）',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/85779/27/7387/209920/5dfaf1b8E384fdc7e/1f780f0827469c66.jpg!q80.dpg'
+        },
+        {
+          goodsName:'卡乐比（Calbee）波塔比系列 海苔味薯片68g*2 印尼进口零食 休闲膨化食品 薯条薯片',
+          goodsAttr:'蜂蜜黄油奶酪味薯片60g*2',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/156722/24/27746/210792/61c0394bE773d9e10/8bfe6bf044ae6c8d.jpg!q80.dpg'
+        },
+        {
+          goodsName:'港荣蒸蛋糕 奶香味900g整箱 饼干蛋糕营养早餐小面包休闲零食糕点',
+          goodsAttr:'全蛋奶不加水奶香蒸蛋糕900g/箱',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/210380/4/27387/132825/636df95aEb963d0b1/7b3f8ff279b0881d.jpg!q80.dpg'
+        },
+        {
+          goodsName:'乐事薯片超值分享装210g 零食大礼包（新老包装交替发货）',
+          goodsAttr:'红烩味+烧烤味+大波浪鱿鱼味',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/141913/3/30685/145242/635b9275Eec6c25d5/84eb7ee185d9dc24.jpg!q80.dpg'
+        },
+        {
+          goodsName:'【2022新品】华为适用蓝牙耳机无线双耳运动跑步游戏低延迟苹果vivo荣耀oppo小米手机通用固质 ',
+          goodsAttr:'白色升级版【改名+开盖即连+HIFI音质+不闪灯】',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/70046/16/21423/55532/6329e0e7Ed2b8a42f/8ec061710bba0acc.jpg!q80.dpg'
+        }
+      ]
+    },
+    {
+      type:2,
+      data:[
+        {
+          dateTime:'2022-11-12',
+          goodsName:'三星（SAMSUNG）27英寸 曲面 可壁挂 HDMI接口 节能爱眼认证 FreeSync CF39 电脑办公显示器 C27F390FHC',
+          goodsAttr:'【爆款】27英寸/IPS/可壁挂/爱眼',
+          statusTxt:'公开',
+          appraise:'这个商品真的太好的 值得推荐',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/91332/25/20825/72373/636a2eaeEed718e2d/da3a0ba41a74f7c0.jpg!q80.dpg'
+        },
+        {
+          dateTime:'2022-11-12',
+          goodsName:'AOCSXM 24英寸直面2K75HZ游戏电竞电脑显示器165HZ升降旋转IPS台式液晶 24寸直黑2K75HZ HDR400 升降 旋转',
+          goodsAttr:'24寸直黑2K75HZ HDR400 升降 旋转',
+          statusTxt:'公开',
+          appraise:'这个商品真的太好的 值得推荐',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/86249/37/31587/88005/6356b694E3e298a4d/329f8a4b57975b76.jpg!q80.dpg'
+        },
+        {
+          dateTime:'2022-11-12',
+          goodsName:'AOC 23.8英寸 IPS 2K高清 微边框 低蓝光不闪 双HDMI接口 人体工学支架可升降 液晶电脑显示器 Q2490PXQ',
+          goodsAttr:'27英寸/2K/IPS/广色域/三边微框',
+          appraise:'这个商品真的太好的 值得推荐',
+          statusTxt:'公开',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/212903/39/23728/117727/636e6dd7E617279a6/86c45e5b36554bfc.jpg!q80.dpg'
+        },
+        {
+          dateTime:'2022-11-12',
+          goodsName:'【11.11返场钜惠】 施华洛世奇蓝调天鹅 DAZZLING SWAN 项链 璀璨 镀白金色 5521074',
+          goodsAttr:'镀白金色 5521074',
+          statusTxt:'公开',
+          appraise:'这个商品真的太好的 值得推荐',
+          img:'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/132462/21/31327/91297/636e7b15E1ee4affd/e609a3a8a0103411.jpg!q80.dpg'
+        }
+      ]
+    }
+  ]
+  if(!!req){
+     const item = maps.find(v => v.type === Number( req.query.type))
+     if(!!item){
+        res_data.data = item.data
+     }
+  };
+  res.send(res_data)
+})
 module.exports = router;
